58bb6ef97b6c5ac134554a700eb81b01fb863ec1
Matthias Wright <matthias.wright@yahoo.de>
2021-08-27 19:35:39 +0200

    update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='jax-fid',
-      version='0.0.1',
+      version='0.0.2',
       url='https://github.com/matthias-wright/jax-fid',
       author='Matthias Wright',
       packages=find_packages(),
