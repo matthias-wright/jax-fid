bdf681aca326ac5cfdee2f23d0bc75c24a75bc76
Boris Dayma <boris.dayma@gmail.com>
2022-05-25 23:29:48 +0000

    feat: use CPU  for pre-processing

--- jax_fid/fid.py
+++ jax_fid/fid.py
@@ -1,37 +1,38 @@
 import jax
 import jax.numpy as jnp
-import flax
-import flax.linen as nn
 import numpy as np
 from PIL import Image
 import os
-import functools
 import scipy
 from tqdm import tqdm
 
-from . import inception
-from . import utils
-
 
 def compute_statistics(path, params, apply_fn, batch_size=1, img_size=None):
-    if path.endswith('.npz'):
+    if path.endswith(".npz"):
         stats = np.load(path)
-        mu, sigma = stats['mu'], stats['sigma']
+        mu, sigma = stats["mu"], stats["sigma"]
         return mu, sigma
 
     images = []
-    for f in os.listdir(path):
+    for f in tqdm(os.listdir(path)):
         img = Image.open(os.path.join(path, f))
-        img = jnp.array(img) / 255.0
-        if img_size is not None:
-            img = jax.image.resize(img, shape=(img_size[0], img_size[1], img.shape[2]), method='bilinear', antialias=False)
+        # convert if only a single channel
+        if img.mode == "L":
+            img = img.convert("RGB")
+        # resize if not the right size
+        if img_size is not None and img.size[:2] != img_size:
+            img = img.resize(
+                size=(img_size[0], img_size[1]),
+                resample=Image.BILINEAR,
+            )
+        img = np.array(img) / 255.0
         images.append(img)
-    images = jnp.array(images)
-    
-    num_batches = int(np.ceil(images.shape[0] / batch_size))
+
+    num_batches = int(len(images) // batch_size)
     act = []
     for i in tqdm(range(num_batches)):
-        x = images[i * batch_size:i * batch_size + batch_size]
+        x = images[i * batch_size : i * batch_size + batch_size]
+        x = np.asarray(x)
         x = 2 * x - 1
         pred = apply_fn(params, jax.lax.stop_gradient(x))
         act.append(pred.squeeze(axis=1).squeeze(axis=1))
@@ -56,8 +57,10 @@ def compute_frechet_distance(mu1, mu2, sigma1, sigma2, eps=1e-6):
 
     covmean, _ = scipy.linalg.sqrtm(sigma1.dot(sigma2), disp=False)
     if not np.isfinite(covmean).all():
-        msg = ('fid calculation produces singular product; '
-               'adding %s to diagonal of cov estimates') % eps
+        msg = (
+            "fid calculation produces singular product; "
+            "adding %s to diagonal of cov estimates"
+        ) % eps
         print(msg)
         offset = np.eye(sigma1.shape[0]) * eps
         covmean = scipy.linalg.sqrtm((sigma1 + offset).dot(sigma2 + offset))
@@ -66,11 +69,8 @@ def compute_frechet_distance(mu1, mu2, sigma1, sigma2, eps=1e-6):
     if np.iscomplexobj(covmean):
         if not np.allclose(np.diagonal(covmean).imag, 0, atol=1e-3):
             m = np.max(np.abs(covmean.imag))
-            raise ValueError('Imaginary component {}'.format(m))
+            raise ValueError("Imaginary component {}".format(m))
         covmean = covmean.real
 
     tr_covmean = np.trace(covmean)
-    return (diff.dot(diff) + np.trace(sigma1) + np.trace(sigma2) - 2 * tr_covmean)
-
-
-
+    return diff.dot(diff) + np.trace(sigma1) + np.trace(sigma2) - 2 * tr_covmean
