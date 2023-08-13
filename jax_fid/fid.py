f840335ab4da49c68c74bec45ef28b0a1003b296
hayden-donnelly <donnellyhd@outlook.com>
2023-08-13 17:34:45 -0400

    Test5

--- jax_fid/fid.py
+++ jax_fid/fid.py
@@ -27,6 +27,7 @@ def compute_statistics_with_mmap(path, mmap_filname, params, apply_fn, batch_siz
 
     with open(mmap_filname, 'w+b') as f:
         file_size = np.dtype(dtype).itemsize * activation_dim * num_batches * batch_size
+        print(file_size)
         f.write(b"\0" * file_size)
     print("testtest3")
     #return
