751a1012977c30938411398e84a8ab3d9aa2c3fb
Hayden Donnelly <donnellyhd@outlook.com>
2023-08-14 06:14:16 -0400

    feat: Compute FID stats with mmap for large datasets (#4)
    * Added mmap computation
    
    * Fixed typo
    
    * Create file of correct size before mmap.
    
    * Test
    
    * Test2
    
    * Test3
    
    * Test4
    
    * Test5
    
    * Test6
    
    * Test7
    
    * Test8
    
    * Cleanup and added assertion
    
    * Added tensorflow to requirements
    
    * ..
    
    * ..
    
    * ..
    
    * Removed class mode specification

--- jax_fid/__main__.py
+++ jax_fid/__main__.py
@@ -19,6 +19,8 @@ def main():
     parser.add_argument('--precompute', action='store_true', help='If True, pre-compute statistics for given image directory.')
     parser.add_argument('--img_dir', type=str, help='Path to image directory for pre-computing statistics.')
     parser.add_argument('--out_dir', type=str, help='Path where pre-computed statistics are stored.')
+    parser.add_argument('--mmap', action='store_true', default=False, help='If true, use mmap to compute statistics.')
+    parser.add_argument('--mmap_file', type=str, help='Name of mmap file. Only used if mmap is true.')
     args = parser.parse_args()
 
     rng = jax.random.PRNGKey(0)
@@ -31,7 +33,10 @@ def main():
     if args.precompute:
         assert args.img_dir is not None, 'img_dir must be specified if precompute_stats is True'
         assert args.out_dir is not None, 'out_dir must be specified if precompute_stats is True'
-        mu, sigma = fid.compute_statistics(args.img_dir, params, apply_fn, args.batch_size, args.img_size)
+        if args.mmap:
+            mu, sigma = fid.compute_statistics_with_mmap(args.img_dir, args.mmap_file, params, apply_fn, args.batch_size, args.img_size)
+        else:
+            mu, sigma = fid.compute_statistics(args.img_dir, params, apply_fn, args.batch_size, args.img_size)
         os.makedirs(args.out_dir, exist_ok=True)
         np.savez(os.path.join(args.out_dir, 'stats'), mu=mu, sigma=sigma)
         print('Saved pre-computed statistics at:', os.path.join(args.out_dir, 'stats.npz'))
@@ -40,8 +45,12 @@ def main():
         assert args.path2 is not None, 'path2 must be specified'
         assert os.path.isdir(args.path1) or args.path1.endswith('.npz'), 'path1 must be a directory or an .npz file'
         assert os.path.isdir(args.path2) or args.path2.endswith('.npz'), 'path2 must be a directory or an .npz file'
-        mu1, sigma1 = fid.compute_statistics(args.path1, params, apply_fn, args.batch_size, args.img_size)
-        mu2, sigma2 = fid.compute_statistics(args.path2, params, apply_fn, args.batch_size, args.img_size)
+        if args.mmap:
+            mu1, sigma1 = fid.compute_statistics_with_mmap(args.path1, args.mmap_file, params, apply_fn, args.batch_size, args.img_size)
+            mu2, sigma2 = fid.compute_statistics_with_mmap(args.path2, args.mmap_file, params, apply_fn, args.batch_size, args.img_size)
+        else:
+            mu1, sigma1 = fid.compute_statistics(args.path1, params, apply_fn, args.batch_size, args.img_size)
+            mu2, sigma2 = fid.compute_statistics(args.path2, params, apply_fn, args.batch_size, args.img_size)
 
         fid_score = fid.compute_frechet_distance(mu1, mu2, sigma1, sigma2, eps=1e-6)
         print('Fid:', fid_score)
